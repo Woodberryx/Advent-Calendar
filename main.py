ac117a09f1e1f67c178436771e1747232b0a695a
Woodberryx <Kovildin97@gmail.com>
2023-12-04 23:21:09 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types.web_app_info import WebAppInfo
+
+bot = Bot('6596148336:AAGkW5knNiqS5fKXZivjC4w51hYtzIFkiG4', parse_mode='HTML')
+dp = Dispatcher(bot=bot)
+
+#Keyboards
+acm = InlineKeyboardMarkup()
+acm.add(InlineKeyboardButton(text='Advent Calendar IT MY HUB', web_app=WebAppInfo(url='file:///C:/Users/Kovil/PycharmProjects/adventcalendarbot/index.html')))
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await message.answer('<b>Давайте начнем!🎄</b>\n'
+                         'Чтобы начать, нажмите на кнопку ниже.',
+                         reply_markup=acm)
+
+if __name__ == '__main__':
+    executor.start_polling(dp)

e4d6e32e314bc3fc393728460f5a91ce71cb5781
Woodberryx <Kovildin97@gmail.com>
2023-12-05 23:33:04 +0300

    Initial Commitsdasdвава

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ dp = Dispatcher(bot=bot)
 
 #Keyboards
 acm = InlineKeyboardMarkup()
-acm.add(InlineKeyboardButton(text='Advent Calendar IT MY HUB', web_app=WebAppInfo(url='https://api.render.com/deploy/srv-cln3q8nfeb2c73egd0eg?key=L7aGrycYS9A')))
+acm.add(InlineKeyboardButton(text='Advent Calendar IT MY HUB', web_app=WebAppInfo(url='https://alexdev.rathole.icu/index.html')))
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
